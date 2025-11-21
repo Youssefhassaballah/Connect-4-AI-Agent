73dc919e2c13aa09754a8330a863e12c0ac2014e
AbdElrahman Elsayed <abdonawar1234567@gmail.com>
2025-11-21 14:10:23 +0200

    Refactor code structure for improved readability and maintainability

--- backend/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-ee_console(tree)
-
-    # print("\nApplying AI move...")
-    # game.apply_move(best_move, 2)
-
-    # print("\nBoard after AI move:")
-    # for row in game.get_board():
-    #     print(row)
